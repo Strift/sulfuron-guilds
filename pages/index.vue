97fd909bd6e3726197b865eb67f1e972fb38b9dc
Strift <lau.cazanove@gmail.com>
2020-11-14 16:45:00 +0100

    Add search bar template

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <FactionLayout>
-    <div class="font-semibold text-gray-400 text-4xl mb-2">
-      Recrutement <span class="italic">vraiment</span> actif
+    <div class="text-gray-400 mb-3 flex">
+      <div class="font-semibold text-2xl">
+        Guildes
+      </div>
+      <input v-model="search" type="text" class="ml-auto px-3 rounded-full text-gray-700" placeholder="Nyk Trib">
     </div>
-    <div class="text-white mb-12 border-t border-white pt-3 opacity-75">
-      Ces guildes mettent à jour leur recrutement très fréquemment.
+    <div class="text-white mb-12 border-t border-white pt-3 opacity-75 flex justify-end space-x-4">
+      <label v-for="wowClass in wowClasses" :key="wowClass" class="flex items-center space-x-1">
+        <input :id="wowClass" type="checkbox" :name="wowClass">
+        <ClassIcon :wow-class="wowClass" class="h-5" />
+        <!-- <div>{{ wowClass }}</div> -->
+      </label>
     </div>
 
     <div class="grid grid-flow-row grid-cols-3 gap-12">
@@ -25,14 +32,18 @@
 <script>
 import FactionLayout from '~/layouts/FactionLayout.vue'
 
+import ClassIcon from '~/components/icons/ClassIcon.vue'
 import GuildCard from '~/components/GuildCard.vue'
 
 export default {
   components: {
     FactionLayout,
+    ClassIcon,
     GuildCard
   },
   data: () => ({
+    search: '',
+    wowClasses: ['Druid', 'Hunter', 'Mage', 'Paladin', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior'],
     guilds: [{
       name: 'Les Croisés de Pandragon',
       type: 'Classique',
