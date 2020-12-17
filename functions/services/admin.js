732b5876c9c8463f752cdd6cc0a815d1366d6109
Strift <lau.cazanove@gmail.com>
2020-12-17 20:03:25 +0100

    Update service account path

--- functions/services/admin.js
+++ functions/services/admin.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin')
 
-const serviceAccount = require('../ServiceAccount.json')
+const serviceAccount = require('../.service-account.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // databaseURL: 'https://sulfuron-guilds.firebaseio.com'
-  databaseURL: 'http://localhost:9000/?ns=sulfuron-guilds'
+  databaseURL: 'https://sulfuron-guilds.firebaseio.com'
+  // databaseURL: 'http://localhost:9000/?ns=sulfuron-guilds'
 })
 
 module.exports = admin
