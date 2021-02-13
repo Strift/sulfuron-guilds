f6933eebcb61eec6e6aa345acef3d493ca3d15d8
Strift <lau.cazanove@gmail.com>
2021-02-13 16:58:56 +0100

    Fix logo proportions on index page

--- components/GuildCard.vue
+++ components/GuildCard.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
     <div class="bg-gray-900 p-5 flex items-center rounded-t-lg">
-      <img :src="logoUrl" alt="" class="h-12 mr-5">
+      <div
+        class="h-12 w-12 mr-5 items-center justify-center flex rounded overflow-hidden"
+      >
+        <img
+          v-if="hasLogo"
+          :src="logoUrl"
+          :alt="`Logo ${name}`"
+          class="w-full"
+        >
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="text-gray-800"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      </div>
       <div class="text-gray-500 font-semibold text-lg leading-6">
         {{ name }}
       </div>
@@ -67,6 +86,9 @@ export default {
     // supports: { type: Number, required: true }
   },
   computed: {
+    hasLogo () {
+      return this.logoUrl !== null
+    },
     readableDays () {
       return this.raidDays.map(day => day.slice(0, 3)).join(', ')
     },
