29eb2c13ae85896702652dbec548568184a0b821
Strift <lau.cazanove@gmail.com>
2021-02-14 16:40:03 +0100

    Update admin page design

--- pages/admin.vue
+++ pages/admin.vue
@@ -6,13 +6,21 @@
     <div class="space-y-10">
       <InformationCard class="space-y-2">
         <p class="font-semibold">
-          Cette page est en construction. 🛠️
-        </p>
-        <p>
-          Attention, il n'y a aucune vérification de l'existence des comptes !
+          🛠️ Cette page est en construction.
         </p>
       </InformationCard>
-      <PageSectionTitle>Créer une guilde</PageSectionTitle>
+      <PageSectionTitle>🆕 Créer une guilde</PageSectionTitle>
+      <InformationCard class="space-y-2">
+        <p class="font-semibold">
+          ⚠ Attention
+        </p>
+        <p>
+          Il n'y a pas de vérification de l'existence des comptes Battle.net, faites attention aux minuscules et majuscules.
+        </p>
+        <p>
+          Les GMs ne peuvent pas modifier le nom de leur guilde, faites bien attention à l'orthographe.
+        </p>
+      </InformationCard>
       <div class="max-w-sm space-y-10">
         <FormInput
           v-model="guild"
@@ -28,20 +36,30 @@
           Valider
         </PrimaryButton>
       </div>
-      <PageSectionTitle>Guildes (draft)</PageSectionTitle>
-      <div v-if="listOfDraftGuilds.length" class="bg-gray-300 text-gray-800 p-6 rounded shadow">
-        {{ listOfDraftGuilds }}
+      <PageSectionTitle>📝 Guildes non publiées</PageSectionTitle>
+      <div v-if="draftGuilds.length" class="bg-gray-300 text-gray-800 p-6 rounded shadow">
+        <ul class="list-disc list-inside">
+          <li v-for="(guild, i) in draftGuilds" :key="i">
+            {{ guildName(guild) }} <span class="text-sm text-gray-600">({{ guild.ownerUid }})</span>
+          </li>
+        </ul>
       </div>
-      <PageSectionTitle>Guildes (publiques)</PageSectionTitle>
-      <div v-if="listOfPublicGuilds.length" class="bg-gray-300 text-gray-800 p-6 rounded shadow">
-        {{ listOfPublicGuilds }}
+      <PageSectionTitle class="flex justify-between items-baseline">
+        ✅ Guildes publiées <span class="text-gray-600 text-base">{{ publishedGuilds.length }} guildes</span>
+      </PageSectionTitle>
+      <div v-if="publishedGuilds.length" class="bg-gray-300 text-gray-800 p-6 rounded shadow">
+        <ul class="list-disc list-inside">
+          <li v-for="(guild, i) in publishedGuilds" :key="i">
+            {{ guildName(guild) }} <span class="text-sm text-gray-600">({{ guild.ownerUid }})</span>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
 
 import WOW_CLASSES from '~/data/classes.json'
 
@@ -55,23 +73,10 @@ export default {
     error: null
   }),
   computed: {
-    ...mapState({
-      guilds: state => state.guilds.list
-    }),
-    listOfDraftGuilds () {
-      return this.guilds
-        .filter(({ published }) => !published)
-        .map(({ name, ownerUid }) => `${name} (${ownerUid})`)
-        .sort()
-        .join(', ')
-    },
-    listOfPublicGuilds () {
-      return this.guilds
-        .filter(({ published }) => published)
-        .map(({ name, ownerUid }) => `${name} (${ownerUid})`)
-        .sort()
-        .join(', ')
-    }
+    ...mapGetters('guilds', [
+      'draftGuilds',
+      'publishedGuilds'
+    ])
   },
   async mounted () {
     // Performing data fetching in mounted hook because of NuxtFirebase issues with SSR
@@ -104,10 +109,11 @@ export default {
         this.error = err
       }
     },
-    head () {
-      return {
-        title: 'Administration'
+    guildName (guild) {
+      if (guild.faction && guild.faction !== '') {
+        return `[${guild.faction.charAt(0)}] ${guild.name}`
       }
+      return `[?] ${guild.name}`
     }
   },
   head () {
--- store/guilds.js
+++ store/guilds.js
@@ -10,21 +10,23 @@ export const actions = {
   enableSync: firestoreAction(function ({ bindFirestoreRef }) {
     const ref = this.$fire.firestore
       .collection('guilds')
-      .where('published', '==', true)
+      // .where('published', '==', true)
 
     return bindFirestoreRef('list', ref, { wait: true })
   }),
   disableSync: firestoreAction(function ({ unbindFirestoreRef }) {
     unbindFirestoreRef('list', false)
-  }),
-  add: firestoreAction(function (ctx, guild) {
-    const ref = this.$fire.firestore.collection('guilds').doc()
-    return ref.set(guild)
   })
 }
 
 export const getters = {
   currentFactionGuilds (state, getters, rootState) {
-    return state.list.filter(guild => guild.faction === rootState.faction)
+    return state.list.filter(guild => guild.published && guild.faction === rootState.faction)
+  },
+  draftGuilds (state) {
+    return state.list.filter(guild => guild.published === false)
+  },
+  publishedGuilds (state) {
+    return state.list.filter(guild => guild.published)
   }
 }
