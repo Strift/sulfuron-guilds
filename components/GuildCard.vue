f3cc010048c036c46b36eb42bc7e0950829f5186
Strift <lau.cazanove@gmail.com>
2021-02-28 00:11:20 +0100

    Add testing for GuildCard component

--- components/GuildCard.vue
+++ components/GuildCard.vue
@@ -2,8 +2,14 @@
   <div>
     <div class="p-5 flex items-center rounded-t-lg bg-gray-800">
       <div class="h-12 w-12 mr-6 items-center justify-center flex rounded overflow-hidden">
-        <img v-if="hasLogo" :src="logoUrl" :alt="`Logo ${name}`" class="w-full">
-        <EmptyGuildLogo v-else class="text-gray-700" />
+        <img
+          v-if="hasLogo && !loadingError"
+          :src="logoUrl"
+          :alt="`Logo ${name}`"
+          class="w-full"
+          @error="loadingError = true"
+        >
+        <MissingGuildLogo v-else class="text-gray-700" />
       </div>
       <div class="text-gray-400 font-semibold text-lg leading-6">
         {{ name }}
@@ -48,7 +54,19 @@
 </template>
 
 <script>
+import MissingGuildLogo from '~/components/MissingGuildLogo.vue'
+import CalendarIcon from '~/components/icons/CalendarIcon.vue'
+import ClockIcon from '~/components/icons/ClockIcon.vue'
+import ClassIcon from '~/components/icons/ClassIcon.vue'
+
 export default {
+  name: 'GuildCard',
+  components: {
+    MissingGuildLogo,
+    CalendarIcon,
+    ClockIcon,
+    ClassIcon
+  },
   props: {
     name: { type: String, required: true },
     type: { type: String, required: true },
@@ -61,9 +79,13 @@ export default {
     // activity: { type: Number, required: true },
     // supports: { type: Number, required: true }
   },
+  data: () => ({
+    loadingError: false
+  }),
   computed: {
     hasLogo () {
-      return this.logoUrl !== '' && this.logoUrl != null
+      return this.logoUrl !== '' &&
+        this.logoUrl != null
     },
     readableDays () {
       return this.raidDays.map(day => day.slice(0, 3)).join(', ')
