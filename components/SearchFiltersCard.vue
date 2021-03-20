28ffdf76d6b5f67af203f59a142924932c77ec96
Strift <lau.cazanove@gmail.com>
2021-03-20 16:28:00 +0100

    Class specs filters working

--- components/SearchFiltersCard.vue
+++ components/SearchFiltersCard.vue
@@ -6,11 +6,26 @@
     <div class="space-y-2">
       <div v-for="wowClass in wowClasses" :key="wowClass.value" class="space-y-2">
         <FormCheckBox
+          v-show="!isClassSelected(wowClass.value)"
+          :id="wowClass.value"
+          :key="wowClass.value"
+          :name="wowClass.value"
+          :checked="isClassSelected(wowClass.value)"
+          @change="updateSelectedClasses(wowClass.value, wowClass.specs, $event)"
+        >
+          <div class="flex space-x-2 items-center">
+            <ClassIcon :wow-class="wowClass.value" class="h-5" />
+            <span class="font-semibold text-gray-700">{{ wowClass.name }}</span>
+          </div>
+        </FormCheckBox>
+        <FormCheckBox
           v-for="spec in wowClass.specs"
+          v-show="isClassSelected(wowClass.value)"
           :id="inputName(wowClass, spec)"
           :key="spec.value"
           :name="inputName(wowClass, spec)"
-          :checked="false"
+          :checked="isSpecSelected(wowClass.value, spec.value)"
+          @change="updateSelectedSpecs(wowClass.value, spec.value, $event)"
         >
           <div class="flex space-x-2 items-center">
             <ClassIcon :wow-class="wowClass.value" class="h-5" />
@@ -44,11 +59,45 @@ export default {
     ClassIcon
   },
   data: () => ({
+    selectedClasses: [],
+    selectedSpecs: {},
     wowClasses: WOW_CLASSES.sort(sortByName)
   }),
+  created () {
+    WOW_CLASSES.forEach((wowClass) => {
+      this.$set(this.selectedSpecs, wowClass.value, [])
+    })
+  },
   methods: {
     inputName (wowClass, spec) {
       return `${wowClass.value}-${spec.value}`
+    },
+    updateSelectedClasses (classValue, classSpecs, checked) {
+      if (checked) {
+        this.selectedClasses.push(classValue)
+        this.selectedSpecs[classValue] = classSpecs.map(spec => spec.value)
+      }
+    },
+    updateSelectedSpecs (classValue, specValue, checked) {
+      if (checked) {
+        this.selectedSpecs[classValue].push(specValue)
+      } else {
+        const index = this.selectedSpecs[classValue].indexOf(specValue)
+        this.selectedSpecs[classValue].splice(index, 1)
+        if (this.selectedSpecs[classValue].length === 0) {
+          this.removeSelectedClass(classValue)
+        }
+      }
+    },
+    removeSelectedClass (classValue) {
+      const index = this.selectedClasses.indexOf(classValue)
+      this.selectedClasses.splice(index, 1)
+    },
+    isClassSelected (classValue) {
+      return this.selectedClasses.includes(classValue)
+    },
+    isSpecSelected (classValue, specValue) {
+      return this.selectedSpecs[classValue].includes(specValue)
     }
   }
 }
