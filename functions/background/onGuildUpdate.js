bcbe99ed68cfac92f335f18f2c2e4d53f5fb3bb4
Strift <lau.cazanove@gmail.com>
2021-07-11 20:07:40 +0200

    Fix function return

--- functions/background/onGuildUpdate.js
+++ functions/background/onGuildUpdate.js
@@ -1,8 +1,14 @@
+const logger = require('../services/logger')
 const firebase = require('../services/firebase')
 const FirestoreFieldValue = firebase.firestore.FieldValue
 
 module.exports = (snap, context) => {
-  return snap.after.ref.set({
-    updatedAt: FirestoreFieldValue.serverTimestamp()
-  })
+  // Only trigger this if something else than `updatedAt` was updated
+  if (snap.before.data().updatedAt !== snap.after.data().updatedAt) {
+    return Promise.resolve()
+  } else {
+    return snap.after.ref.set({
+      updatedAt: FirestoreFieldValue.serverTimestamp()
+    }, { merge: true })
+  }
 }
