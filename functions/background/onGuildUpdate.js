fc59d1243d82212de27913be075304de73fe541e
Strift <lau.cazanove@gmail.com>
2021-07-12 09:15:36 +0200

    Add logs

--- functions/background/onGuildUpdate.js
+++ functions/background/onGuildUpdate.js
@@ -1,11 +1,14 @@
+const logger = require('../services/logger')
 const firebase = require('../services/firebase')
 const FirestoreFieldValue = firebase.firestore.FieldValue
 
 module.exports = (snap, context) => {
+  logger.debug('Guild was updated', snap)
   // Only trigger this if something else than `updatedAt` was updated
   if (snap.before.data().updatedAt !== snap.after.data().updatedAt) {
     return Promise.resolve()
   } else {
+    logger.info(`Updated updatedAt on ${snap}`)
     return snap.after.ref.set({
       updatedAt: FirestoreFieldValue.serverTimestamp()
     }, { merge: true })
