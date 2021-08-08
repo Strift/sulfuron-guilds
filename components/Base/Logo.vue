c462865263dcaa6074bff98f1d91b943d586c25d
Strift <lau.cazanove@gmail.com>
2021-08-08 21:29:04 +0200

    Replace new logo

---
+++ components/Base/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center space-x-2">
+    <nuxt-img
+      src="/images/new-logo.png"
+      alt="Icone WoW Sulfuron"
+      height="64"
+      width="64"
+    />
+    <span class="font-light text-2xl text-blue-200 tracking-widest uppercase">Guildes</span>
+  </div>
+</template>
