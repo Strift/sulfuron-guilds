9928fb9c5d7e4638c8d74d2701d2590d8c67ffc5
Strift <lau.cazanove@gmail.com>
2021-08-18 19:57:45 +0200

    Add default firebase project ID

--- nuxt.config.js
+++ nuxt.config.js
@@ -139,7 +139,7 @@ export default {
       apiKey: 'AIzaSyDgCfIFFy-5Lw8rQ-HF3M--T-oT270LdpE',
       authDomain: 'sulfuron-guilds.firebaseapp.com',
       databaseURL: 'https://sulfuron-guilds.firebaseio.com',
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID || 'sulfuron-guilds',
       storageBucket: 'sulfuron-guilds.appspot.com',
       messagingSenderId: '229682010576',
       appId: '1:229682010576:web:db892c4df7f3ba3b1281d5'
